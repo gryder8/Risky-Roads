83219406fe5f8a979aab1e53c3c164a2d97c967a
gryder8 <gavin.ryder82@gmail.com>
2021-11-13 15:29:59 -0800

    buttons on bottom

---
+++ Inrix Hacks/RouteSelectorVC.swift
@@ -0,0 +1,17 @@
+//
+//  RouteSelectorVC.swift
+//  Inrix Hacks
+//
+//  Created by Gavin Ryder on 11/13/21.
+//
+
+import UIKit
+import Foundation
+
+class RouteSelectorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- Inrix Hacks/ViewController.swift
+++ Inrix Hacks/ViewController.swift
@@ -8,39 +8,36 @@
 import UIKit
 import MapKit
 
+
+
 class ViewController: UIViewController, MKMapViewDelegate {
+
 
     
     //MARK: Oulets
     @IBOutlet weak var mapView: MKMapView! //mapKit view
     @IBOutlet var superView: UIView!
     
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         setupMap()
+        //DispatchQueue.main.async {
+        //}
     }
     
     func setupMap() {
-        setupMapConstraints(mapView);
         let centerCoord:CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.778, -122.441)
         let region = MKCoordinateRegion.init(center: centerCoord, latitudinalMeters: 9200, longitudinalMeters: 9200)
         mapView.setRegion(region, animated: true)
     }
-    
-    func setupMapConstraints(_ mapView: MKMapView) {
-//        let bounds:UILayoutGuide = superView.safeAreaLayoutGuide
-//        mapView.leadingAnchor.constraint(equalTo: bounds.leadingAnchor).isActive = true
-//        mapView.bottomAnchor.constraint(equalTo: bounds.bottomAnchor).isActive = true
-//        mapView.topAnchor.constraint(equalTo: bounds.topAnchor).isActive = true
-//        mapView.trailingAnchor.constraint(equalTo: bounds.trailingAnchor).isActive = true
-        mapView.bounds = superView.bounds
-    }
-    
-
-
-
 }
+
 
