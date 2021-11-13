c2c7639f95d3e24200549d621d6e6f23e0217535
gryder8 <gavin.ryder82@gmail.com>
2021-11-13 12:55:01 -0800

    map zooms in and shows

--- Inrix Hacks/ViewController.swift
+++ Inrix Hacks/ViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    
+    //MARK: Oulets
+    @IBOutlet weak var mapView: MKMapView! //mapKit view
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet var superView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupMap()
     }
+    
+    func setupMap() {
+        let centerCoord:CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.778, -122.441)
+        let region = MKCoordinateRegion.init(center: centerCoord, latitudinalMeters: 9200, longitudinalMeters: 9200)
+        mapView.setRegion(region, animated: true)
+        mapView
+    }
+    
+
 
 
 }
