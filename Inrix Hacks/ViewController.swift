9fb4635d67074a8607e9962ea36b8b6626540c88
gryder8 <gavin.ryder82@gmail.com>
2021-11-13 11:43:21 -0800

    Initial Commit

---
+++ Inrix Hacks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inrix Hacks
+//
+//  Created by Gavin Ryder on 11/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
